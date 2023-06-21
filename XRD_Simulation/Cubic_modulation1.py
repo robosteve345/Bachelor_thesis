11487e89a8c9a85953ed0cfe632d33544441eb24
robosteve <86156294+robosteve345@users.noreply.github.com>
2023-06-21 18:51:38 +0200

    A lot happened

--- XRD_Simulation/Cubic_modulation1.py
+++ XRD_Simulation/Cubic_modulation1.py
@@ -26,13 +26,12 @@ def translation(x, storage_x, storage_y, storage_z, i):
     storage_y.append(x_transl[1])
     storage_z.append(x_transl[2])
 
-def Structurefactor(k2d, l2d, h, Unitary, Amplitude, q_cdw, noisefactor, f_list, noise=True):
+def Structurefactor(Atom1, Atom2, k2d, l2d, h, Unitary, Amplitude, q_cdw, n, noisefactor, f_list, noise=True):
     """Atomic positions"""
-    x_Atom1, y_Atom1, z_Atom1 = [0], [0], [0] # must be deined as lists
-    x_Atom2, y_Atom2, z_Atom2 = [0.5], [0.5], [0.5]
+    x_Atom1, y_Atom1, z_Atom1 = [0],[0],[0] #Atom1[0], Atom1[1], Atom1[2] # must be defined as lists
+    x_Atom2, y_Atom2, z_Atom2 = [0.5], [0.5], [0.5] #Atom2[0], Atom2[1], Atom2[2]
     """Atomic positions modulation: commensurate with q_CDW = 0.2*2*pi/a of atom 2 (1/2, 1/2, 1/2)"""
     # Full translation of Atom1 & Atom2 for 6 unit cells
-    n = 5 # # of unit cells, should be q_cdw**-1
     for i in range(1, n):  # begins at 1 because unit cell 0 is already given
         translation(np.array([x_Atom1[0], y_Atom1[0], z_Atom1[0]]), x_Atom1, y_Atom1, z_Atom1, i)  # Atom1
         translation(np.array([x_Atom2[0], y_Atom2[0], z_Atom2[0]]), x_Atom2, y_Atom2, z_Atom2, i)  # Atom2
@@ -42,12 +41,6 @@ def Structurefactor(k2d, l2d, h, Unitary, Amplitude, q_cdw, noisefactor, f_list,
     # Final atomic positions
     Atom1, Atom2 = np.array([x_Atom1, y_Atom1, z_Atom1]), np.array([x_Atom2, y_Atom2, z_Atom2]),
     print("Atom1_z={}, Atom2_z={}".format(np.asarray(Atom1)[2, :], np.asarray(Atom2)[2, :]))
-    plt.plot(1 / 2 * np.ones(n) + np.arange(0, n, 1), np.ones(n), label='equilibrium', ls='', marker='x')
-    plt.xlabel('Unit cell')
-    plt.ylabel('z')
-    plt.plot(Atom2[2], np.ones(n), label='distorted', ls='', marker='o')
-    plt.legend()
-    plt.show()
     """Scattering amplitudes F"""
     # Form factors
     F_Atom1_list, F_Atom2_list = [], []
@@ -74,17 +67,22 @@ def Structurefactor(k2d, l2d, h, Unitary, Amplitude, q_cdw, noisefactor, f_list,
         I = I + noisefactor*np.max(I) * np.random.rand(len(k2d), len(k2d))  # Add random noise with maximum noisefactor
     else:
         pass
-    return F, I
+    print(Atom2)
+    print(I)
+    return F, I, Atom1, Atom2, n
 
-def excludekpoints(deltak, I, k2d, l2d, kmax):
+def excludekpoints(deltak, I, k2d, l2d, kmax, n):
     ##############################################################################
-    # Excluding unallowed K-points (ONLY FOR deltak=/1)
+    # # Excluding unallowed K-points (ONLY FOR deltak=/1)
     k_intlist = np.arange(0, len(k2d), 1)  # erstelle indices aller k-Werte
-    for i in range(0, 2 * kmax + 1):  # for kmax=lmax=2 and ∆k=0.1, up to 2*kmax=lmax + 1
-        k_intlist = np.delete(k_intlist, i * 9)  # 9 for ∆k=0.1 and kmax=lmax=2, 99 for ∆k=0.01 and kmax=lmax=2
+    print(k_intlist)
+    for i in range(0, (2 * kmax + 1)):  # LEAVES ONLY INTEGER K-values
+        #print(range(0,2*kmax+1))
+        k_intlist = np.delete(k_intlist, i * 9)  # 9 for ∆k=0.1 , 99 for ∆k=0.01, 9 because, the list gets one less each time
+        #print(k_intlist)
     for i in k_intlist:  # Set unallowed K-values for intensities to 0
         I[:, i] = 0
-    # Exluding unallowed L-points (ONLY FOR deltak=0.01 or deltak=0.001)
+    # # Exluding unallowed L-points
     # l_intlist = np.arange(0, len(l2d), 1)  # erstelle indices aller l-Werte
     # if deltak == 0.1:
     #     for i in range(0, 2 * kmax + 1):
@@ -96,22 +94,31 @@ def excludekpoints(deltak, I, k2d, l2d, kmax):
     #         l_intlist = np.delete(l_intlist, i * 9)  # Lösche jeden zehnten index
     #     for i in l_intlist:  # Set unallowed L-values for intensities to 0
     #         I[i, :] = 0
+
+
     return I
     ##############################################################################
 
 
-def plotfunction(k2d, l2d, h, k0, l0, kmax, lmax, I, k, Amplitude, savefig=False):
-    figure(figsize=(9, 7), dpi=100)
+def plotfunction(k2d, l2d, h, k0, l0, kmax, lmax, I, k, n, Amplitude, Atom1, Atom2, savefig=False):
+    figure(figsize=(13, 7), dpi=100)
     plt.suptitle(r"Body centered cubic (bcc) with sinusoidal modulation of Atom2=(1/2,1/2,1/2)"
     #"Body centered cubic (bcc), F($\mathbf{Q}$)=$f(1 + e^{-i\pi(h+k+l)})$ \n $I=f^2(2+2\cos(\pi(h+k+l))), f=1$")
     # I_fcc = f^2(4 + 2*(\cos(\pi(h+l)) + \cos(\pi(h-l)) + \cos(\pi(k+h)) + \cos(\pi(k-h))".format(h)
     )
     plt.subplot(2, 2, 1)
-    plt.title('Countourplot')
-    plt.contourf(l2d, k2d, I, cmap='viridis', extent=(k0 - kmax, k0 + kmax, l0 - lmax, l0 + lmax))
-    plt.colorbar()
-    plt.xlabel("K(rlu)")
-    plt.ylabel("L(rlu)")
+    # print(Atom2)
+    plt.scatter(1 / 2 * np.ones(n) + np.arange(0, n, 1), np.ones(n), label=r'Atom2=$(\frac{1}{2},\frac{1}{2},\frac{1}{2})$ equilibrium', facecolors='none',
+                edgecolors='orange', s=100)
+    plt.xlabel('z')
+    plt.ylabel('')
+    plt.scatter(Atom2[2,:], np.ones(n), label='Atom2=$(0.5,0.5,0.5+{} \sin(2\cdot 1/{} \pi L)$ distorted'.format(Amplitude, n), marker='o')
+    plt.legend()
+    # plt.title('Countourplot')
+    # plt.contourf(l2d, k2d, I, cmap='viridis', extent=(k0 - kmax, k0 + kmax, l0 - lmax, l0 + lmax))
+    # plt.colorbar()
+    # plt.xlabel("K(rlu)")
+    # plt.ylabel("L(rlu)")
 
     plt.subplot(2, 2, 2)
     plt.title("Gaussian interpolation")
@@ -134,12 +141,9 @@ def plotfunction(k2d, l2d, h, k0, l0, kmax, lmax, I, k, Amplitude, savefig=False
     plt.tight_layout()
 
     plt.subplot(2, 2, 4)
-    # plt.title(r'$I(L)=f^2(4 + 2*(\cos(\pi(h+l)) + \cos(\pi(h-l)) + \cos(\pi(k+h)) + \cos(\pi(k-h))+\cos(\pi(k+l)) +\cos(\pi(k-l))), f=1$')
-    plt.plot(l2d[:, 0], I[:, 0], ls='--', marker='.', label='K={}'.format(np.round(k[0], 2)))
-    # plt.plot(l2d[:, 0], I[:, int(len(k)/2)*deltak], ls='--', marker='.', label='K={}'.format(np.round(int(len(k)/2)*deltak, 2)))
-    plt.plot(l2d[:, 0], I[:, -1], ls='--', marker='.', label='K={}'.format(np.round(k[-1], 2)))
-    # plt.plot(l2d[:, 0], I[:, -7], ls='--', marker='.', label='K={}'.format(np.round(k[-7], 2)))
-    # plt.plot(l2d[:, 0], I[:, -4], ls='--', marker='.', label='K={}'.format(np.round(k[-4], 2)))
+    plt.title(r'$I(L)=f^2(4 + 2*(\cos(\pi(h+l)) + \cos(\pi(h-l)) + \cos(\pi(k+h)) + \cos(\pi(k-h))+\cos(\pi(k+l)) +\cos(\pi(k-l))), f=1$')
+    plt.plot(l2d[:, 0], I[:, 0], ls='--', marker='.', label='K={}'.format(np.round(k[0], 2)), ms=0.1)
+    plt.plot(l2d[:, 0], I[:, -1], ls='--', marker='.', label='K={}'.format(np.round(k[-1], 2)), ms=0.1)
     plt.legend(loc='lower center')
     plt.ylabel(r"Intensity $I\propto F(\mathbf{Q})^2$")
     plt.xlabel("L(rlu)")
@@ -153,18 +157,23 @@ def plotfunction(k2d, l2d, h, k0, l0, kmax, lmax, I, k, Amplitude, savefig=False
 
 def main():
     print(__doc__)
+    ############################################
+    Atom1 = [[0],[0],[0]]
+    Atom2 = [[0.5], [0.5], [0.5]]
+    ############################################
     k0, l0, kmax, lmax = 0, 0, 2, 2  # boundaries of K and L for the intensity maps
     deltak = 0.1  # or 0.1, k-space point distance
     h = 1  # H value in k space
-    Amplitude = 0.02 # Distortion amplitude
+    Amplitude = 0.10 # Distortion amplitude
     q_cdw = 0.2  # Distortion periodicity in 2pi/a
+    n = int(q_cdw**(-1))  # # of unit cells, should be q_cdw**-1
     noisefactor = 0.0 # Corrects noise to noisefactor*np.max(I)
     # Electronic form factors
     f_list = [1, 1] # [Atom1, Atom2]
-    k2d, l2d, k, Unitary = kspacecreator(k0,l0,kmax, lmax,deltak)
-    F, I = Structurefactor(k2d, l2d, h, Unitary, Amplitude, q_cdw, noisefactor, f_list, noise=True)
-    I = excludekpoints(deltak, I, k2d, l2d, kmax)
-    plotfunction(k2d, l2d, h, k0, l0, kmax, lmax, I, k, Amplitude, savefig=True)
+    k2d, l2d, k, Unitary = kspacecreator(k0, l0, kmax, lmax, deltak)
+    F, I, Atom1, Atom2, n = Structurefactor(Atom1, Atom2, k2d, l2d, h, Unitary, Amplitude, q_cdw, n, noisefactor, f_list, noise=True)
+    I = excludekpoints(deltak, I, k2d, l2d, kmax, n)
+    plotfunction(k2d, l2d, h, k0, l0, kmax, lmax, I, k, n, Amplitude, Atom1, Atom2, savefig=True)
     maincubic()
 if __name__ == '__main__':
     main()
